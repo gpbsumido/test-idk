495d31361f799d25542337a6350770eeddd9f510
gpbsumido <gpbsumido@gmail.com>
2023-02-14 12:57:37 -0800

    cleanup

--- dist/gameClient.d.ts
+++
@@ -1,8 +0,0 @@
-import { Base } from "./base";
-import { Game } from "./game";
-import { NFT } from "./nft";
-declare class GameClient extends Base {
-}
-interface GameClient extends Game, NFT {
-}
-export default GameClient;
--- dist/gameClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const base_1 = require("./base");
-const game_1 = require("./game");
-const utils_1 = require("./utils");
-class GameClient extends base_1.Base {
-}
-(0, utils_1.applyMixins)(GameClient, [game_1.Game]);
-exports.default = GameClient;
